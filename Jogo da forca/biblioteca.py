be4258945a3a6db6433e820de6ad4546f341e826
Arthur Aguiar <101827286+imthur@users.noreply.github.com>
2023-10-16 23:36:44 -0300

    Update biblioteca.py

--- Jogo da forca/biblioteca.py
+++ Jogo da forca/biblioteca.py
@@ -1,7 +1,7 @@
-frutas = ['MORANGO', 'ABACAXI', 'MAMAO', 'PERA', 'UVA', 'MANGA', 'LARANJA', 'MACA', 'BANANA', 'KIWI', 'ABACATE', 'ABACAXI', 'AMEIXA', 'MELANCIA', 'PESSEGO', 'NECTARINA', 'LIMAO', 'LIMA', 'FIGO', 'COCO']
+frutas = ['MORANGO', 'ABACAXI', 'MAMAO', 'PERA', 'UVA', 'MANGA', 'LARANJA', 'MACA', 'BANANA', 'KIWI', 'ABACATE', 'ABACAXI', 'AMEIXA', 'MELANCIA', 'PESSEGO', 'LIMAO', 'FIGO', 'COCO']
 objetos = ['FACA', 'TESOURA', 'LAPIS', 'PORTA', 'CADEIRA', 'MESA', 'CANETA', 'CADERNO', 'CELULAR', 'TELEVISAO', 'CARRO', 'BICICLETA', 'ANEL', 'OCULOS', 'RELOGIO', 'CHAVE', 'MARTELO', 'COLHER', 'GARRAFA', 'XICARA']
 paises = ['BRASIL', 'VENEZUELA', 'ESPANHA', 'INGLATERRA', 'COLOMBIA', 'CUBA', 'CHINA', 'INDIA', 'INDONESIA', 'PAQUISTAO', 'NIGERIA', 'RUSSIA', 'MEXICO', 'JAPAO', 'ETIOPIA', 'EGITO', 'VIETNA', 'TAILANDIA', 'ARGENTINA', 'PERU']
-profissoes = ['MEDICO', 'ENGENHEIRO', 'ADVOGADO', 'PROFESSOR', 'MUSICO', 'ARTISTA', 'ATLETA', 'COZINHEIRO', 'BOMBEIRO', 'POLICIAL', 'ELETRICISTA', 'FOTOGRAFO', 'CIENTISTA', 'PSICOLOGO', 'JORNALISTA', 'ARQUITETO', 'PILOTO', 'VETERINARIO', 'DENTISTA']
+profissoes = ['MEDICO', 'ENGENHEIRO', 'ADVOGADO', 'PROFESSOR', 'MUSICO', 'ARTISTA', 'ATLETA', 'COZINHEIRO', 'BOMBEIRO', 'POLICIAL', 'ELETRICISTA', 'FOTOGRAFO', 'CIENTISTA', 'PSICOLOGO', 'JORNALISTA', 'ARQUITETO', 'FARMACEUTICO', 'PILOTO', 'VETERINARIO', 'DENTISTA']
 esportes = ['FUTEBOL', 'BASQUETE', 'TENIS', 'NATACAO', 'VOLEI', 'GINASTICA', 'ATLETISMO', 'HANDEBOL', 'CICLISMO', 'SURF', 'ESCALADA', 'SKATE', 'GOLFE', 'JUDO', 'BOXE', 'FUTSAL', 'MMA', 'BEISEBOL', 'RUGBY']
 def boneco(chances):
     if chances == 6:
@@ -9,12 +9,48 @@ def boneco(chances):
     if chances == 5:
         return f'_____\n|  O\n| \n|'
     if chances == 4:
-        return f'_____\n|  O\n|   |\n|'
+        return f'_____\n|  O\n|  |\n|'
     if chances == 3:
         return f'_____\n|  O\n| /|\n|'
     if chances == 2:
-        return f'___\n   O \n| /|\ \n'
+        return f'_____\n|  O\n| /|\ \n|'
     if chances == 1:
         return f'_____\n|  O\n| /|\ \n| / '
     if chances == 0:
         return f'_____\n|  O\n| /|\ \n| /\ '
+
+def score(usuario, vitorias, derrotas):
+    tam = 30
+
+    pontuacao = {
+        "Usuário": usuario,
+        "Acertos": vitorias,
+        "Erros": derrotas,
+    }
+    while True:
+        print(f'+{"-" * tam}+')
+        print(f'|{"PONTUAÇÃO":^{tam}}|')
+        print(f'+{"-" * tam}+')
+        for k, v in pontuacao.items():
+            print(f'|{f" {k} - {v}":{tam}}|')
+        print(f"+{'-' * tam}+")
+        break
+
+def cabecI():
+    tam = 30
+
+    while True:
+        print(f'+{"-" * tam}+')
+        print(f'|{"JOGO DA FORCA":^{tam}}|')
+        print(f'+{"-" * tam}+')
+        print(f'|{f"      Seja bem vindo(a)":{tam}}|')
+        print(f"+{'-' * tam}+")
+        break
+def cabec():
+    tam = 30
+
+    while True:
+        print(f'+{"-" * tam}+')
+        print(f'|{"JOGO DA FORCA":^{tam}}|')
+        print(f'+{"-" * tam}+')
+        break

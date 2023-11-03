b39e48297da555902a78a2444f36e5e357d6c022
Arthur Aguiar <101827286+imthur@users.noreply.github.com>
2023-11-02 21:22:42 -0300

    Update Heranças.py

--- Heranças/Heranças.py
+++ Heranças/Heranças.py
@@ -30,35 +30,3 @@ class Coelho(Animal):
         super().__init__(nome, cor)
     def emitirSom(self):
         print(f'O {self.nome} ronronou.')
-
-class Forma():
-    def __init__(self):
-        self.area = 0
-        self.perimetro = 0
-
-class Triângulo(Forma):
-    def __init__(self, lado):
-        super().__init__(area, perimetro)
-        self.lado = lado
-
-    def calculaArea(self):
-        self.area = (self.lado * self.lado) / 2
-        print(f'A área do triângulo é: {self.area}')
-
-    def calculaPerimetro(self):
-        self.perimetro = self.lado * 3
-        print(f'O perímetro do triângulo é: {self.area}')
-
-class Retangulo(Forma):
-    def __init__(self, altura, base):
-        super().__init__()
-        self.altura = altura
-        self.base = base
-
-    def calculaArea(self):
-        self.area = self.base * self.altura
-        print(f'A área do retângulo é: {self.area}')
-
-    def calculaPerimetro(self):
-        self.perimetro = 2 * (self.base + self.altura)
-        print(f'O perímetro do retângulo é: {self.area}')
